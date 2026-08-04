de9d14610df4a16d33aca8009e60e53e5a4327ba
Salim-raza <rezasalim00011@gmail.com>
2026-08-04 20:56:40 +0600

    feat: add signal for automatic profile creation

---
+++ profiles/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=100, blank=True, null=True)
+    last_name = models.CharField(max_length=100, blank=True, null=True)
+    image = models.ImageField(upload_to="profile_image", null=True, blank=True)
+    division = models.CharField(max_length=250, null=True, blank=True)
+    district = models.CharField(max_length=250, null=True, blank=True)
+    upazila = models.CharField(max_length=250, null=True, blank=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    room_number = models.CharField(max_length=10, blank=True, null=True)
+    date_of_birth = models.DateField(blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.user.email
---
+++ profiles/signals.py
@@ -0,0 +1,16 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import Profile
+from accounts.models import CustomUser
+
+
+
+@receiver(post_save, sender=CustomUser)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+        
+@receiver(post_save, sender=CustomUser)
+def save_profile(sender, instance, **kwargs):
+    Profile.objects.get_or_create(user=instance)
+    instance.profile.save()
