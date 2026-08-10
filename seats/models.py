172881066ac1e25332cb25a7a3bf6cce95738224
Salim-raza <rezasalim00011@gmail.com>
2026-08-10 22:09:33 +0600

    feat: add seat model

--- seats/models.py
+++ seats/models.py
@@ -1,4 +1,17 @@
 from django.db import models
-
+from django.conf import settings
 
 # Create your models here.
+class Seat(models.Model)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    room_number = models.CharField()
+    status = models.CharField(
+        max_length=20,
+        choices=[
+            ("available", "Available"),
+            ("occupied", "Occupied"),
+        ],
+        default="available"
+    )
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
