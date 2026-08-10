9ff5140c5dfb159fed008588e57e3de3dde849fd
Salim-raza <rezasalim00011@gmail.com>
2026-08-10 22:06:54 +0600

    feat: add seat app

---
+++ seats/views.py
@@ -0,0 +1,6 @@
+from rest_framework.decorators import APIView, permission_classes, authentication_classes
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework import status
+# Create your views here.
+
+class 
