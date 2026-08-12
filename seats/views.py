37c0c7f659fa9e426194df17b3e774146b588401
Salim-raza <rezasalim00011@gmail.com>
2026-08-12 21:01:35 +0600

    feat: add seat update api:

--- seats/views.py
+++ seats/views.py
@@ -1,5 +1,6 @@
 from rest_framework.decorators import APIView, permission_classes, authentication_classes
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from seats.models import Seat
 from .serializers import SeatCreateSerializers
 from accounts.permission import IsController
 from rest_framework.response import Response
@@ -13,7 +14,7 @@ class SeatAdd(APIView):
     @swagger_auto_schema(
         method='post',
         request_body=SeatCreateSerializers,
-        responses={200: SeatCreateSerializers(), 400: "bad request"}
+        responses={200: SeatCreateSerializers(), 400: "bad request"},
         operation_description="seat add"
     )
     def post(self, request, format=None):
@@ -21,5 +22,25 @@ class SeatAdd(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response({"message": "seta add successfully", "data": serializer.data}, status=status.HTTP_201_CREATED)
+    
+    
+class SeatUpdate(APIView):
+    permission_classes = [IsController]
+    authentication_classes = [JWTAuthentication]
+    @swagger_auto_schema(
+        method='put',
+        request_body=SeatCreateSerializers,
+        responses={200: SeatCreateSerializers(), 400: "bad request"},
+        operation_description="seat update"
+    )
+    def put(self, request, format=None):
+        seat_id = request.data.get("id")
+        seat = Seat.objects.filter(id=seat_id).first()
+        if not seat:
+            return Response({"message": "Seat not found"}, status=status.HTTP_404_NOT_FOUND)
+        serializer = SeatCreateSerializers(seat, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({"message": "Seat updated successfully", "data": serializer.data}, status=status.HTTP_200_OK)
         
     
