2f2440b15ee5959672a7d5cdceec9f7529be7281
Salim-raza <rezasalim00011@gmail.com>
2026-08-18 17:44:16 +0600

    feat: add meal model

--- meals/models.py
+++ meals/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Meal(models.Model):
+    MEAL_TYPES = [
+        ('breakfast', 'Breakfast'),
+        ('lunch', 'Lunch'),
+        ('dinner', 'Dinner'),
+    ]
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    date = models.DateField()
+    meal_type = models.CharField(max_length=20, choices=MEAL_TYPES)
+    is_taken = models.BooleanField(default=False)
+    create_at = models.DateTimeField(auto_now_add=True)
+    
+    
+    def __str__(self):
+        return f"{self.user.email} - {self.data} - {self.meal_type}"
--- meals/views.py
+++ meals/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from rest_framework.decorators import APIView
+from rest_framework import status
+from rest_framework.response import Response
+from accounts.permission import
 
 # Create your views here.
