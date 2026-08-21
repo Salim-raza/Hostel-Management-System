3ce6d75d89ca6cba2c85ab646dab1aae3d648548
Salim-raza <rezasalim00011@gmail.com>
2026-08-21 14:50:39 +0600

    feat: add fine update api

--- fines/urls.py
+++ fines/urls.py
@@ -1,2 +1,8 @@
 from django.urls import path
 from .views import *
+
+
+urlpatterns = [
+    path('add/', FineAddView.as_view(), name='fine-add'),
+    path('update/<int:fine_id>/', FineUpdateview.as_view(), name='fine-update'),
+]
--- fines/views.py
+++ fines/views.py
@@ -1,14 +1,16 @@
 from rest_framework.decorators import APIView, permission_classes, authentication_classes
+from rest_framework.authentication import JWTAuthentication
+from accounts.permission import IsControllerOrAssistantController
+from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from .serializers import FineAddSerializer
-from rest_framework.authentication import JWTAuthentication
 from rest_framework import status
 from .models import Fine
 
 # Create your views here.
 class FineAddView(APIView):
     authentication_classes = [JWTAuthentication]
-    permission_classes = []
+    permission_classes = [IsControllerOrAssistantController]
 
     def post(self, request):
         serializer = FineAddSerializer(data=request.data)
@@ -16,3 +18,16 @@ class FineAddView(APIView):
             serializer.save()
             return Response({"message": "Fine added successfully."}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class FineUpdateview(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsControllerOrAssistantController]
+
+    def patch(self, request, fine_id):
+        fine = get_object_or_404(Fine, id=fine_id)
+        serializer = FineAddSerializer(fine, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "Fine updated successfully."}, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
